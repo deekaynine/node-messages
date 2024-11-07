54edfe84617b051fc729cfcf69553951ce9144a0
deekaynine123 <deekaynine@gmail.com>
2024-11-07 02:44:31 -0500

    user mock app

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require("express")
-const authorRouter = require("./routes/authorRouter")
-const bookRouter = require("./routes/bookRouter")
+const userRouter = require("./routes/userRouter")
 
 require("dotenv").config()
 const path = require("node:path");
@@ -18,53 +17,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 // routers
-app.use('/authors', authorRouter)
-app.use('/books', bookRouter)
-
-const links = [
-    { href: "/", text: "Home" },
-    { href: "about", text: "About" },
-  ];
-
-  const messages = [
-    {
-      text: "Hi there!",
-      user: "Amando",
-      added: new Date().toLocaleDateString()
-    },
-    {
-      text: "Hello World!",
-      user: "Charles",
-      added: new Date().toLocaleDateString()
-    }
-  ];
-
-// root routes
-app.get("/", (req, res) => {
-    res.render("index", { messages:messages, links: links });
-  });
-
-app.get("/new",(req,res)=>{
-    res.render("form")
-})
-
-app.post("/new",(req,res)=>{
-    const {authorName, message} = req.body
-    messages.push({ text: message, user: authorName, added: new Date().toLocaleTimeString() });
-    res.redirect("/")
-})
-  
-app.get('*', (req, res)=>{
-    res.send("Unknown Router ERROR")
-})
-
-app.get('/:username/messages',(req,res)=>{
-    res.json({
-        params: req.params,
-        query : req.query
-    })
-
-})
+app.use("/", userRouter)
 
 const PORT = process.env.PORT || 3000;
 
